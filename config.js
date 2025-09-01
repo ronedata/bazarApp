d53060e50ccd774a940491971ea5e9d1f2b43718
Md Sakil Mahmud <152467518+ronedata@users.noreply.github.com>
2025-09-01 13:33:19 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Set your Apps Script Web App URL after deployment
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwPxbG6-zhu8uSJh-APlOjyS4Xs4MW_mqVwKsxlpxjX3jAEG87EZeug48vMuZ0EwIta/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzifSRz7e70ClvNYWmpx6-c88t3a63gHRusrkVlyJiSemLt9AxgOhaSbrYos5b25AnE_Q/exec';
